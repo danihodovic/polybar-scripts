3ab51259d81f09783ea901b8973c7fbab9c4c9e9
Dani Hodovic <dani.hodovic@gmail.com>
2021-11-08 16:32:10 +0100

    Clear taskwarrior status if no task active

--- polybar-scripts/taskwarrior/taskwarrior.sh
+++ polybar-scripts/taskwarrior/taskwarrior.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 task="$(task +ACTIVE -DELETED export rc.json.array=off 2> /dev/null)"
 task_description=$(echo "$task" | jq -r '"\(.description)(\(.id))"')
-echo "%{F#00ff00}✔ $task_description"
+if [ -n "$task_description" ]; then
+  echo "%{F#00ff00}✔ $task_description"
+else
+  echo
+fi
